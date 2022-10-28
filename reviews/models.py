168833c60ea3bd4dde75e368e8fbf462b2cc9af1
soohofather <enthchal@gmail.com>
2022-10-28 11:45:13 +0900

    게시판 이미지 업로드 완료

--- reviews/models.py
+++ reviews/models.py
@@ -15,6 +15,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, upload_to="images/")
 
 
 class Comment(models.Model):
