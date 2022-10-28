b441ee02120415b44cfc14b147406142b331daf2
yoosoonil <y2k7654@gmail.com>
2022-10-28 11:45:29 +0900

    이미지 업로드 기능추가/비밀번호 변경 구현중

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,31 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
   
   class Meta:
     model = get_user_model()
-    fields = ('username', 'password1', 'password2', )
+    fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'image', )
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('username', )
+        fields = ('first_name', 'last_name', )
+        
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomPasswordChangeForm, self).__init__(*args, **kwargs)
+        self.fields['old_password'].label = '기존 비밀번호'
+        self.fields['old_password'].widget.attrs.update({
+            'class': 'form-control',
+            'autofocus': False,
+        })
+        self.fields['new_password1'].label = '새 비밀번호'
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+        })
+        self.fields['new_password2'].label = '새 비밀번호 확인'
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+        })
--- django_pair5/settings.py
+++ django_pair5/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+MEDIA_ROOT = BASE_DIR / "static" / "images"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
